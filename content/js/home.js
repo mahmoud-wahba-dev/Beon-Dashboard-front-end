e059575d0dd91ea4245824a47676711cc703716c
Mahmoud Wahba <mahmoud.wahba.dev@gmail.com>
2024-08-27 12:46:06 +0300

    add files

---
+++ content/js/home.js
@@ -0,0 +1,12 @@
+// // script.js
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const sidebar = document.getElementById("sidebar");
+//     const mainContent = document.getElementById("main-content");
+//     const sidebarToggleBtn = document.getElementById("sidebarToggle");
+
+//     sidebarToggleBtn.addEventListener("click", function () {
+//         sidebar.classList.toggle("collapsed");
+//         mainContent.classList.toggle("expanded");
+//     });
+// });
